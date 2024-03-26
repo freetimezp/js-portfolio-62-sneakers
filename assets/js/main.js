cb1b1a0beefd220d9952dc2638c079784358cfac
freetimezp <kraftukrnet@ukr.net>
2024-03-26 15:45:28 +0200

    04. js-portfolio-62-sneakers. Responsive.

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,13 @@ swiperImages.controller.control = swiperTitles;
 swiperTitles.controller.control = swiperImages;
 
 
+//change bg header
+const bgHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header');
+}
+
+window.addEventListener('scroll', bgHeader);
 
 
 
